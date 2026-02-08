8fb141e8aac24374f55b3b56b76ebb72fc641418
tanay <110236203+thebluemarble69@users.noreply.github.com>
2026-02-08 22:39:52 +0530

    d

---
+++ Didy.java
@@ -0,0 +1,14 @@
+public class Didy(){
+  public static void main(String[] args){
+    while (true) {
+      System.out.println("Baby,,, shark do do do dodo du do.");
+    
+    }
+
+  }
+
+
+
+
+
+}
